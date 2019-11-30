4d30c39064f7c391ae2830d49d69ee2c0c0c9a15
Henry <hclao1995@gmail.com>
2019-11-29 17:36:48 -0800

    added some documentation

--- cis22c_f19_project/cis22c_f19_project/MathUtil.h
+++ cis22c_f19_project/cis22c_f19_project/MathUtil.h
@@ -11,13 +11,13 @@ namespace MathUtil
 {
 	// holds value of the last generated prime number array
 	// resets at the start of a new generation
-	extern size_t ATKINS_ARRAY_SIZE;
-	const size_t MAX_PRIME = 207239;
+	extern size_t		ATKINS_ARRAY_SIZE;
+	const size_t		MAX_PRIME = 207239;
 	/* Generates an array of prime numbers less than the
 	specified limit using an implementation of the Sieve of Atkin's algorithm
 	@parameter limit bounds the prime number generator
 	@return an array of prime numbers less than limit */
-	size_t* generatePrimesAtkins(size_t limit);
+	size_t*				generatePrimesAtkins(size_t limit);
 	/* This function tests for primality using an implementation
 	of based on the Miller-Rabins Primality Test
 	@param k is an input parameter that determines
@@ -25,25 +25,25 @@ namespace MathUtil
 	@param n is the integer to be tested
 	@return returns false if n is composite and returns true if n
 	is probably prime.   */
-	bool isPrimeMillerRabin(size_t n, int k);
+	bool				isPrimeMillerRabin(size_t n, int k);
 	/* Identify an archived prime that is atleast 2 times greater than target
 	@pre size cannot be greater than 9973 and primes2.txt must exist in
 	root directory unmodified
 	@return prime that is greater than or equal to 2 times argument size */
-	size_t findPrime(size_t size);
+	size_t				findPrime(size_t size);
 	/* Utility function to do modular exponentiation.
 	@param x is the base
 	@param y is the exponenet
 	@param p is the modulus of congruence
 	@return  (x^y) % p */
-	int power(int x, unsigned int y, int p);
+	int					power(int x, unsigned int y, int p);
 	/* This function is called for all k trials. It returns
 	false if n is composite and returns false if n is
 	probably prime.
 	@param d is an odd number such that  d*2^r = n - 1 for some r >= 1
 	@param n is an odd integer to be tested for primality
 	@return true if n is prime else false if n is composite */
-	bool millerTest(int d, int n);
+	bool				millerTest(int d, int n);
 
 }
 #endif // ! _MATH_UTIL_H
--- cis22c_f19_project/cis22c_f19_project/StringUtil.h
+++ cis22c_f19_project/cis22c_f19_project/StringUtil.h
@@ -5,15 +5,31 @@
 #include <locale>
 namespace StringUtil
 {
-	std::string strip(const std::string& str);
-	std::string uppercase(const std::string&);
-	List<std::string>* split(const std::string& str, const std::string delim);
-	std::string join(const List<std::string>& tokens);
-	std::string lowercase(const std::string& str);
-	std::string lstrip(const std::string& str);
-	std::string rstrip(const std::string& str);
-	std::string replace(const std::string& data, const std::string& to_search, const std::string& replace_str);
-	bool containsAlpha(const std::string& data);
+	std::string				strip(const std::string& str);
+	std::string				uppercase(const std::string&);
+	List<std::string>*		split(const std::string& str, const std::string delim);
+	std::string				join(const List<std::string>& tokens);
+	std::string				lowercase(const std::string& str);
+	std::string				lstrip(const std::string& str);
+	std::string				rstrip(const std::string& str);
+	/* Replace all the substrings found (to_search) in the data str
+	returning a modified string without mutating original input string
+	@param data is the string format
+	@param to_search is the substring to find in the data string and replace
+	@param replace_str is the string to replace the found(to_search) substrings 
+	in the data string
+	@pre none
+	@post none
+	@return a string is formatted and reflects the changes from the input args */
+	std::string				replace(const std::string& data, 
+									const std::string& to_search, 
+									const std::string& replace_str);
+	/* Returns true if the string contains any characters of the alphabet 
+	@param data is the string to search
+	@pre none
+	@post none
+	@return true if the string contains a character of the alphabet else false */
+	bool					containsAlpha(const std::string& data);
 };
 #endif // ! _STRING_UTIL
 
