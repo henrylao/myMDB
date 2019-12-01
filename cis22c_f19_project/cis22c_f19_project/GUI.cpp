fdef4a52a3c1952e9c9ed214a2967077a72d71e9
Johnstirfry <john1999820@gmail.com>
2019-11-30 19:09:46 -0800

    gui

--- cis22c_f19_project/cis22c_f19_project/GUI.cpp
+++ cis22c_f19_project/cis22c_f19_project/GUI.cpp
@@ -1,158 +1,60 @@
+#include <iostream>
 #include "GUI.h"
+#include "Movie.h"
+#include "List.h"
+#include "menu.h"
+#include "menu_dialog.h"
 
-bool GUI::isValidName(string name) {
-int size = name.size();
-for (int i = 0; i < size; i++)
-{
-	if (!isalpha(name[i]))
-	{
-		if (isdigit(name[i]))
-		{
-			return false;
-		}
+
+void UI_search(/*const database& database*/) {
+	int choice = menu_prompt("How do you want to search movies by?", menu_search, 6);
+	switch (choice) {
+	case 1: {
+		UI_search_by_title(/*const database& database*/);
+		break;
 	}
-
-}
-return true;
-}
-bool GUI::isValidMonth(string month) {
-int size = month.size();
-if (size != 2)
-	return false;
-if (stoi(month) > 12 || stoi(month) < 1)
-	return false;
-for (int i = 0; i < size; i++)
-{
-	if (isalpha(month[i]))
-	{
-		return false;
+	case 2: {
+		UI_search_by_year(/*const database& database*/);
+		break;
 	}
-
-}
-return true;
-}
-bool GUI::isValidDay(string day) {
-int size = day.size();
-if (size != 2)
-	return false;
-if (stoi(day) > 31 || stoi(day) < 1)
-	return false;
-for (int i = 0; i < size; i++)
-{
-	if (isalpha(day[i]))
-	{
-		return false;
+	case 3: {
+		UI_search_by_ratings(/*const database& database*/);
+		break;
 	}
-
-}
-return true;
-}
-bool GUI::isValidYear(string year) {
-int size = year.size();
-if (size > 4)
-	return false;
-else if (size != 4)
-	return false;
-for (int i = 0; i < size; i++)
-{
-	if (isalpha(year[i]))
-	{
-		return false;
+	case 4: {
+		UI_search_by_genre(/*const database& database*/);
+		break;
 	}
-
-}
-return true;
-}
-void GUI::promptLoadFile(TableDatabase& db)
-{
-	int try_again;
-	bool done_file_load = false;
-	string outfile_prompt = "Enter the file path for an output file\nExample: C:\\data\\output.txt\nHit enter for a default output.txt file to be created in this program's directory\n";
-	string infile_prompt = "Enter the file path for an input file\nExample: C:\\data\\input.txt\nHit enter to use the default input.txt file in this program's directory\n";
-	string user_in = "";
-	string defaultPath = "InputData.txt";
-	ifstream infile;
-	int choice;
-
-	// prompt file path loop
-	while (!done_file_load)
-	{
-		std::cout << infile_prompt;
-		getline(std::cin, user_in);
-		std::cout << "You entered: " << user_in << std::endl;
-		if (user_in.size() == 0)
-		{
-			cout << "Building from " << defaultPath << endl;
-			cout << divider << endl;
-			db.buildDatabase(defaultPath);
-			break;
-		}
-		// user specified file case
-		else if (user_in.size() > 1)
-		{
-			//attempt to open
-			infile.open(user_in, ios::in);
-			if (infile.fail())
-			{
-				std::cout << "I couldn't open the file from the path entered\n";
-				try_again = menu_prompt("Would you like to try again?", menu_continue_program, 2);
-				if (try_again == 2)
-					return;
-			}
-			else {
-				db.buildDatabase(user_in);
-				break;
-			}
-			// prompt for a try again
-			if (try_again == 1) continue;
-		}
-	} // end file prompt loop
-	db.displayStats();
-	//db.displayDatabase();	// DEBUG
-	cout << divider << endl;
-}
-
-void GUI::promptRead(const TableDatabase& db)
-{
-	bool done = false;
-	std::string input;
-	int choiceExit;
-	while (!done)
-	{
-		std::cout << "Please enter any date in the format: YYYY-MM-DD" << std::endl;
-		getline(cin, input);
-		input = StringUtil::strip(input);
-		
-		// valid input case
-		if (!StringUtil::containsAlpha(input))
-		{
-			if (db.found(input))
-			{
-				db.displayPerson(input);
-			}
-			else
-				std::cout << "Invalid data" << std::endl;
-
-			choiceExit = menu_prompt("Would you like to continue searching?", menu_yes_no, 2);
-			if (choiceExit == 2)
-			{
-				break;
-				std::cout << "Exitting...\n";
-			}
-		}
-		// invalid input case
-		else {
-
-			std::cout << "Invalid data" << std::endl;
-
-			choiceExit = menu_prompt("Would you like to continue searching?", menu_yes_no, 2);
-			if (choiceExit == 2)
-			{
-				break;
-				std::cout << "Exitting...\n";
-			}
-		}
-
-		
+	case 5: {
+		UI_search_by_cast(/*const database& database*/);
+		break;
 	}
+	case 6:{
+		return
+	}
+	}
+}
+
+void UI_add(/*const database& database*/) {
+
+}
+
+void UI_remove(/*const database& database*/) {
+
+}
+
+void UI_edit(/*const database& database*/) {
+
+}
+
+void UI_add(/*const database& database*/) {
+
+}
+
+void UI_remove(/*const database& database*/) {
+
+}
+
+void UI_edit(/*const database& database*/) {
+
 }
--- cis22c_f19_project/cis22c_f19_project/GUI.h
+++ cis22c_f19_project/cis22c_f19_project/GUI.h
@@ -1,44 +1,25 @@
 #ifndef _GUI_H
 #define _GUI_H
 
-#include "binary_node.h"
-#include "binary_tree.h"
-#include "binary_search_tree.h"
-
 #include <iostream>
-#include <fstream>
-#include "person.h"
+#include "GUI.h"
+#include "Movie.h"
+#include "List.h"
 #include "menu.h"
 #include "menu_dialog.h"
-#include "TableDatabase.h"
-#include <string>
 
-#include <time.h>
-#include <chrono>
+// Search help funciton
+bool UI_search(/*const database& database*/);
+void UI_search_by_title(/*const database& database*/);
+void UI_search_by_year(/*const database& database*/);
+void UI_search_by_ratings(/*const database& database*/);
+void UI_search_by_genre(/*const database& database*/);
+void UI_search_by_cast(/*const database& database*/);
+// Add help funciton
+void UI_add(/*const database& database*/);
+// Remove help funciton
+void UI_remove(/*const database& database*/);
+// Edit help funciton
+void UI_edit(/*const database& database*/);
 
-namespace GUI
-{
-	bool isValidName(std::string name);
-	bool isValidDay(std::string day);
-	bool isValidMonth(std::string month);
-	bool isValidYear(std::string month);
-	/* Prompt user for a filepath with the option to use the
-	included default file (InputData.txt). The prompt loops until
-	the user either quits or enters a valid file path. 
-	@pre TableDatabase db is empty
-	@post TableDatabase db is populated with data from the specified file path
-	@param db is a reference to an empty TableDatabase to be loaded with the file data */
-	void promptLoadFile(TableDatabase& db);
-	void promptCreate(TableDatabase& db);
-	void promptDelete(TableDatabase& db);
-	void promptUpdate(TableDatabase& db);
-	/* Prompt the user to enter a date(key) to display a Person
-	stored in the database's contained hashtable.
-	If the key is found, the corresponding value is displayed
-	else a message of "Invalid Data" is outputted. The prompt loops
-	until the user decides to exit.
-	@param db is a populated TableDatabase	*/
-	void promptRead(const TableDatabase& db);
-}
-
-#endif // !_GUI_H
+#endif
