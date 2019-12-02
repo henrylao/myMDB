5a07c1fdc74985ab66d172261a2e0c734688d6f5
Henry <hclao1995@gmail.com>
2019-12-01 21:31:28 -0800

    migrated to GUI section to  UI_run_application function

--- cis22c_f19_project/cis22c_f19_project/Main.cpp
+++ cis22c_f19_project/cis22c_f19_project/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "GUI.h"
-//#include "Movie.h"
+#include "Movie.h"
 #include "List.h"
 #include "menu.h"
 #include "menu_dialog.h"
@@ -8,45 +8,4 @@
 using namespace std;
 
 int main() {
-	//string str1 = "ThanksGiving";
-	//List<string> g1;
-	//g1.append("Horror");
-	//g1.append("Comedey");
-	//List<string> c1;
-	//g1.append("Jon Doe");
-	//g1.append("Herry Styles");
-	//cout << c1;
-	//Movie m(str1, 2019, 8.0, &g1, &c1);
-	////cout << m.getGenre() << endl;
-	////cout << m << endl;
-
-	std::cout << "Welcome to NotNetfix!" << std::endl;
-
-	bool end = true;
-	while (end) {
-		int choice = menu_prompt("What would you like to do?", menu_operations, 5);
-		switch (choice) {
-			case 1: {
-				UI_search(/*const database& database*/);
-				break;
-			}
-			case 2: {
-				UI_add(/*const database& database*/);
-				break;
-			}
-			case 3: {
-				UI_remove(/*const database& database*/);
-				break;
-			}
-			case 4: {
-				UI_edit();
-				break;
-			}
-			case 5: {
-				end = false;
-				break;
-			}
-		}
-	}
-	return 0;
 }
