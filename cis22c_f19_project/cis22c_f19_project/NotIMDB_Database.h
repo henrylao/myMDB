0e09d5228ae1986cbf816ab330c23633c252dcb7
Henry <hclao1995@gmail.com>
2019-12-02 13:09:39 -0800

    added update and find/search declarations

--- cis22c_f19_project/cis22c_f19_project/NotIMDB_Database.h
+++ cis22c_f19_project/cis22c_f19_project/NotIMDB_Database.h
@@ -18,8 +18,11 @@ private:
 	HashTable<Actor>					__actorDB;
 	BinarySearchTree<std::string>		__movieNamesBST_byGenre;
 	BinarySearchTree<std::string>		__movieNamesBST_byYear;
-	BinarySearchTree<std::string>		__movieNamesBST_byID;
 
+	BinarySearchTree<std::string>		__movieNamesBST_byID;
+	// ------------------------------------------
+	// Internal Helper Method Section
+	// ------------------------------------------
 	/* Internal functions for managing table populating*/
 	void								__loadMovies(List<Movie>* movies);
 	/* requires that the movies have been loaded to further map a movie to an actor via
@@ -28,34 +31,60 @@ private:
 	void								__buildMovieNamesBST_byID(std::string path);
 	void								__buildMovieNamesBST_byGenre(std::string path);
 	void								__buildMovieNamesBST_byYear(std::string path);
+	void								__clear();
 public:
-	/* Verify movie and actor existence */
-	/* can either verify via BST or movieDB */
-	bool foundMovie(std::string movieName) const;
-	/* verify using actorDB */
-	bool foundActor(std::string actorName) const; 
+	
 	// ------------------------------------------
 	// Constructor & Destructor Section
 	// ------------------------------------------
 	NotIMDB_Database() {}
 	virtual ~NotIMDB_Database();
 	// ------------------------------------------
-	// Create Interface Section
+	// Create/FileIO Section
 	// ------------------------------------------
-	void buildDatabase(std::string actorFilePath, std::string movieFilePa);
+	bool  				loadFromFile(std::string actorFilePath, std::string movieFilePath);
+	// save to a default path or a custom path/name
+	bool  				saveToFile(string t_name = "");
+
 	// ------------------------------------------
-	// Delete Interface Section
+	// Delete Section
 	// ------------------------------------------
+	
 	// ------------------------------------------
-	// Update Interface Section
+	// Update Section
 	// ------------------------------------------
-	bool updateName(std::string key, std::string oldName, std::string newName);
-	bool updateDate(std::string key, std::string oldDate, std::string newDate);
+	// find movie by oldName, hold a copy of the movie delete movie of old name, update copy to newName, finally 
+	// add back into table
+	bool				updateMovieName(std::string oldMovieName, std::string newMovieName);
+	bool				updateMovieYear(std::string key, std::string newYearReleased);
+	bool				updateMovieID(std::string key, std::string newID);
+	bool				updateMovieRuntime(std::string key, std::string newRuntime);
+	// op 0 appends to the genre string, op 1 sets the genre to newGenreName
+	bool				updateMovieGenre(std::string key, std::string newGenreName, int op);
+	bool 				updateMovieRating(std::string key, std::string newKey);
+
+	bool 				updateActorName(std::string oldActorName, std::string newActorName) :
+	bool  				updateActorBirthYear(std::string key, std::string newBirthYear);
+	bool  				updateActorDeathYear(std::string key, std::string newDeathYear);
+	// 0 to append to old string, 1 to set
+	bool  				updateActorRoles(std::string key, std::string newRoleName, int op);
+	bool  				updateActorID(std::string key, std::string newID);
+
+	
 	// ------------------------------------------
-	// Read Interface Section
+	// Search Section
 	// ------------------------------------------
-	void displayActorTableStats() const;
-	void displayMovieTableStats() const;
+	/* Verify movie and actor existence */
+	/* can either verify via BST or movieDB */
+	bool  				foundMovie(std::string movieName) const;
+	/* verify using actorDB */
+	bool  				foundActor(std::string actorName) const;
+
+	// ------------------------------------------
+	// Read Section
+	// ------------------------------------------
+	void 				 displayActorTableStats() const;
+	void  				displayMovieTableStats() const;
 
 };
 
