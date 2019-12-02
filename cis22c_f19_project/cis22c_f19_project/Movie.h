8d8d772ba39e1ad0b0bc8dbbfb682c1d413bc4c5
Henry <hclao1995@gmail.com>
2019-12-02 13:09:21 -0800

    added getters and comparison setters

--- cis22c_f19_project/cis22c_f19_project/Movie.h
+++ cis22c_f19_project/cis22c_f19_project/Movie.h
@@ -23,26 +23,36 @@ private:
 	int						__compareConfig;
 
 public:
+	//----------------------------------------------------
+	// Constructor and Destructor Section
+	//----------------------------------------------------
 	// default comparison is by movie id
 	Movie();
 	Movie(std::string tconstID, std::string title, 
-		std::string year, std::string runTime, const List<std::string>& genres);
+		std::string year, std::string runTime, 
+		const List<std::string>& genres);
+	//----------------------------------------------------
+	// Mutators Section
+	//----------------------------------------------------
+	void		clear();
+	void		setCompareByID() { __compareConfig = 1; }
+	void		setCompareByRating() { __compareConfig = 2; }
+	void		setCompareByYear() { __compareConfig = 3; }
+	void		setCompareByTitle() { __compareConfig = 4; }
+	void		setCompareByRuntime() { __compareConfig = 5; }
 
-	void clear();
-	void readFullInfo() const;
-	void setCompareByID() { __compareConfig = 1; }
-	void setCompareByRating() { __compareConfig = 2; }
-	void setCompareByYear() { __compareConfig = 3;  }
-	void setCompareByTitle() { __compareConfig = 4; }
-	void setCompareByRuntime() { __compareConfig = 5;  }
+	//----------------------------------------------------
+	// Getters Section
+	//----------------------------------------------------
+	void						readFullInfo() const { std::cout << *this << std::endl; }
+	List<std::string>			getGenres() const { return *__genres; }
+	std::string					getTitle() const { return __title;  }
+	std::string					getID() const { return __movieID; }
+	std::string					getRating() const { return __rating; }
 
-	List<std::string> getGenres() const { return *__genres; }
-	std::string getTitle() const { return __title;  }
-	std::string getID() const;
-	//const List<Actor*>* getActors() const;
-	
-	double getRating() const;
-
+	//--------------------------------
+	// Operator Overload Section
+	//------------------------------
 	friend std::ostream& operator<<(std::ostream& out, const Movie& movie);
 	bool operator==(const Movie& movie) const;
 	bool operator<(const Movie& movie) const;
