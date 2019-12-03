8baab9b9d9a41c1835514ac85545b6b101dc8bb5
Henry <hclao1995@gmail.com>
2019-12-02 22:26:25 -0800

    removed broken/unused io functions

--- cis22c_f19_project/cis22c_f19_project/FileIO.cpp
+++ cis22c_f19_project/cis22c_f19_project/FileIO.cpp
@@ -1,118 +1,5 @@
 #include "FileIO.h"
 
-List<Person>* FileIO::buildPersonListFromFile(std::string path)
-{
-	std::ifstream infile;
-	List<Person>* people = new List<Person>();
-	infile.open(path, std::ios::in);
-	List<std::string>* date_tokens;
-	Person* new_person = new Person();
-	if (!infile.good())
-		return nullptr;
-	std::string data;
-
-	// build PersonDatabase
-	while (infile.good())
-	{
-		getline(infile, data, '\n');
-		data = StringUtil::strip(data);
-		// date found case
-		if (isdigit(data[0]))
-		{
-			date_tokens = StringUtil::split(data, "-");
-			new_person->setYear(date_tokens->getEntry(0));
-			new_person->setMonth(date_tokens->getEntry(1));
-			new_person->setDay(date_tokens->getEntry(2));
-			date_tokens->clear();
-			delete date_tokens;
-		}
-		// name found case
-		else if (isalpha(data[0]))
-		{
-			new_person->setName(data);
-		}
-		
-		if (new_person->getBirthday().length() != 2 && new_person->getName().length() != 0)
-		{
-			people->append(*new_person);
-			delete new_person;
-			new_person = new Person();
-		}
-		// reset the date string for next line
-		data = "";
-	}
-	// memory clean up
-	if (new_person->getBirthday().length() == 0 || new_person->getName().length() == 0)
-	{
-		delete new_person;
-	}
-	return people;
-}
-
-
-List<std::string>* FileIO::buildNameListFromFile(std::string path)
-{
-	std::ifstream infile;
-	List<std::string>* names = new List<std::string>();
-	infile.open(path, std::ios::in);
-
-	if (!infile.good())
-		return nullptr;
-
-	std::string name;
-	while (infile.good())
-	{
-		if (getline(infile, name, '\n'))
-		{
-			name = StringUtil::strip(name);
-			if (name.length() != 0)
-			{
-				names->append(name);
-			}
-			// reset after appending or a whitespace has been read
-			name = "";
-		}
-	}
-	return names;
-}
-List<std::string>* FileIO::buildDateListFromFile(std::string path)
-{
-	std::ifstream infile;
-	List<std::string>* dates = new List<std::string>();
-	infile.open(path, std::ios::in);
-	if (!infile.good())
-		return nullptr;
-
-	std::string date;
-	while (infile.good())
-	{
-		if (getline(infile, date, '\n'))
-		{
-			date = StringUtil::strip(date);
-			if (date.length() != 0)
-			{
-				dates->append(date);
-			}
-			// reset after appending or a whitespace has been read
-			date = "";
-		}
-	}
-	return dates;
-}
-
-void FileIO::writeTableToFile(std::string path,
-	const List<std::string>& keys,  HashTable<Person>& table)
-{
-	std::ofstream outfile;
-	outfile.open(path, std::ios::out);
-
-	for (int i = 0; i < keys.getLength(); i++)
-	{
-		outfile << table[keys.getEntry(i)].getName() << std::endl << keys.getEntry(i) << std::endl;
-	}
-	outfile.close();
-}
-
 List<Movie>* FileIO::buildMovieList(std::string path)
 {
 	// attempt open file
@@ -144,7 +31,7 @@ List<Movie>* FileIO::buildMovieList(std::string path)
 			//cout << newMovie << endl;	// DEBUG 
 			data = "";
 			delete dataList;
-			movies->sortedInsert(newMovie);
+			movies->append(newMovie);
 		}
 		
 	}
--- cis22c_f19_project/cis22c_f19_project/FileIO.h
+++ cis22c_f19_project/cis22c_f19_project/FileIO.h
@@ -11,29 +11,6 @@
 namespace FileIO
 {
 	static const std::string BAR_DELIM = "|";
-	/* Builds a list of people from a .txt file. White spaces are 
-	accounted for an do not affect list creation. Order
-	of names and dates must follow the following format:
-	person1_name
-	YYYY-MM-DD
-	person2_name
-	YYYY-MM-DD
-	person3_name
-	YYYY-MM-DD
-	@return pointer to a list of people constructed */
-	List<Person>* buildPersonListFromFile(std::string);
-	/* Builds a list of people from a .txt file of names. 
-	@return pointer to a list of names 
-	@param path is the full path to the target file */
-	List<std::string>* buildNameListFromFile(std::string path);
-	/* Builds a list of dates from a .txt file of dates of
-	the following format:
-	YYYY-MM-DD
-	@return pointer to a list of names
-	@param path is the full path to the target file */
-	List<std::string>* buildDateListFromFile(std::string path);
-	void writeTableToFile(std::string path,
-		const List<std::string>& keys,   HashTable<Person>& table);	
 	/* Reads a .tsv file following the format:
 	tconst | primaryTitle | startYear | runtimeMinutes | genre1, genre2, ... , genre-n 
 	The list is sorted in accordance to tconst, a tag for building the list
@@ -42,9 +19,6 @@ namespace FileIO
 	/* Reads from a .tsv file following the format:
 	nconst | primaryName | birthYear | deathYear | primaryProfession | knownForTitles */
 	List<Actor>* buildActorList(std::string path);
-	/* Function used for multhreading */
-	void loadActorsIntoList(std::string path, List<Actor>* actors);
-	void loadMoviesIntoList(std::string path, List<Movie>* movie);
 	/* Multithread safe function to construct a list of movieTitles from
 	a file containing movieTitles sorted by their IDs */
 	void buildMovieTitles_sortedByID(std::string path, List<std::string>* movieTitles);
