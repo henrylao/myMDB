3db7e539be185cbc347e9101aec2a0f4883247f6
Henry <hclao1995@gmail.com>
2019-12-02 22:26:46 -0800

    Update stack.h

--- cis22c_f19_project/cis22c_f19_project/stack.h
+++ cis22c_f19_project/cis22c_f19_project/stack.h
@@ -1,40 +1,46 @@
 #pragma once
 #include "linked_list.h"
+#include "list.h"
+#ifndef _STACK_H
+#define _STACK_H
+
 
 template <class T>
-class Stack : private LinkedList<T>
+class Stack : private List<T>
 {
 public:
     Stack();
     virtual ~Stack();
     bool push(const T &newItem);
+	size_t size() { return List<T>::getLength(); }
     bool pop();
     T peek() const;
 };
 
 template <class T>
-Stack<T>::Stack() : LinkedList() {}
+Stack<T>::Stack() : List<T>() {}
 
 template <class T>
 Stack<T>::~Stack()
 {
-    clear();
+	List<T>::clear();
 }
 
 template <class T>
 bool Stack<T>::push(const T &newItem)
 {
-    return insert(1, newItem);
+    return List<T>::insert(1, newItem);
 }
 
 template <class T>
 bool Stack<T>::pop()
 {
-    return remove(1);
+    return List<T>::remove(1);
 }
 
 template <class T>
 T Stack<T>::peek() const
 {
-    return getEntry(1);
+    return List<T>::getEntry(1);
 }
+#endif // !_STACK_H
